69c87b80ae0be5b93a7d6e9f5de257ed10ee5f95
MrPotatoXx <salinasgiovanni25@gmail.com>
2023-02-07 00:51:32 -0300

    Corrección del cálculo de porcentaje de progreso en el tiempo

--- progressbar.py
+++ progressbar.py
@@ -2,24 +2,22 @@ import datetime
 import calendar
 from tqdm import tqdm
 
-def calcular_progreso(dia, hora, minuto):
-    minutos_transcurridos = (dia - 1) * minutos_dia + hora * 60 + minuto
-    progreso = minutos_transcurridos / minutos_ano * 100
+def calcular_progreso(dia, mes, ano, hora, minuto):
+    fecha_inicio_ano = datetime.datetime(ano, 1, 1)
+    fecha_actual = datetime.datetime(ano, mes, dia, hora, minuto)
+    delta = fecha_actual - fecha_inicio_ano
+    minutos_transcurridos = delta.total_seconds() / 60
+    progreso = minutos_transcurridos / 525600 * 100
     return progreso
 
 ahora = datetime.datetime.now()
 dia = ahora.day
+mes = ahora.month
+ano = ahora.year
 hora = ahora.hour
 minuto = ahora.minute
 
-if calendar.isleap(ahora.year):
-	minutos_ano = 366 * 24 * 60
-else :
-	minutos_ano = 365 * 24 * 60
-
-minutos_dia = 24 * 60
-
-progreso = calcular_progreso(dia, hora, minuto)
+progreso = calcular_progreso(dia, mes, ano, hora, minuto)
 
 with tqdm(total=100, bar_format="{percentage:3.4f}%|{bar}|100%") as barra:
     barra.update(progreso)
